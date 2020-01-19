d3703627671568c9d4756356454aeb52c5a86b50
Arturo Ortega <macarthur.or@gmail.com>
2020-01-19 11:45:20 -0600

    translations enabled - parse server docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,10 +38,10 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'server/main', label: 'Server'},
+    {doc: 'parse-server/main', label: 'Server'},
     {doc: 'js/main', label: 'JS'},
     {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {blog: true, label: 'Blog'}
   ],
 
   // If you have users set above, you add it here:
